57f6f7c8c705ff8be823fff3a33f9c01ec4a4fd6
ddbogdanov <dennis.d.bogdanov@gmail.com>
2021-04-07 04:25:33 -0500

    Begin input validation

--- src/main/java/com/cougarneticit/gims/controller/admin/EmployeeSceneController.java
+++ src/main/java/com/cougarneticit/gims/controller/admin/EmployeeSceneController.java
@@ -18,12 +18,15 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 @FxmlView("/EmployeeSceneController.fxml")
@@ -39,7 +42,7 @@ public class EmployeeSceneController extends GIMSController implements Initializ
     @FXML private AnchorPane pane;
     @FXML private JFXComboBox<User> userComboBox;
     @FXML private JFXTextField nameTextField, phoneTextField, emailTextField;
-    @FXML private Label nameHelpLabel, phoneHelpLabel, emailHelpLabel; //TODO: Hide labels on unfocus also just make form look better
+    @FXML private Label nameHelpLabel, phoneHelpLabel, emailHelpLabel;
     @FXML private JFXButton addEmployeeButton;
 
     public EmployeeSceneController(FxWeaver fxWeaver) {
@@ -54,8 +57,31 @@ public class EmployeeSceneController extends GIMSController implements Initializ
 
         addEmployeeButton.setOnAction(e -> {
             //TODO: Sanitize input + error handling
-            Employee emp = new Employee(UUID.randomUUID(), userComboBox.getSelectionModel().getSelectedItem(), nameTextField.getText(), phoneTextField.getText(), emailTextField.getText());
-            employeeRepo.save(emp);
+            User activeUser = userComboBox.getSelectionModel().getSelectedItem();
+            String name = nameTextField.getText();
+            String phone = phoneTextField.getText();
+            String email = emailTextField.getText();
+
+            boolean isNameValid = validateName(name);
+            boolean isPhoneValid = validatePhone(phone);
+            boolean isEmailValid = validateEmail(email);
+
+            if (isNameValid && isPhoneValid && isEmailValid) {
+                Employee emp = new Employee(UUID.randomUUID(), activeUser, name, phone, email);
+                employeeRepo.save(emp);
+            }
+            else {
+                //TODO: Change color + revert to original text once fixed.
+                if(!isNameValid) {
+                    nameHelpLabel.setText("Invalid format: First Last");
+                }
+                else if(!isPhoneValid) {
+                    phoneHelpLabel.setText("Invalid format: xxx-xxx-xxxx");
+                }
+                else {
+                    emailHelpLabel.setText("Invalid format: email@domain.com");
+                }
+            }
         });
 
         //Focus listeners for text fields. Show/Hide on focus/un-focus
@@ -70,6 +96,21 @@ public class EmployeeSceneController extends GIMSController implements Initializ
             userList.addAll(userRepo.findAll());
         }
         userComboBox.getItems().addAll(userList);
+    }
+    private boolean validateEmail(String email) {
+        EmailValidator emailValidator = EmailValidator.getInstance();
+        boolean isValid = emailValidator.isValid(email);
+        return isValid;
+    }
+    private boolean validateName(String name) {
+        //TODO
+        return true;
+    }
+    private boolean validatePhone(String phone) {
+        //TODO
+        Pattern pattern = Pattern.compile("^(\\d{3}[-]?){2}\\d{4}$");
+        Matcher matcher = pattern.matcher(phone);
+        return matcher.matches();
     }
 
     public AnchorPane getScene() { return pane; }
