eaa158fac56bb8d605129d7f16994c4a9dd8a263
ddbogdanov <dennis.d.bogdanov@gmail.com>
2021-04-09 05:09:17 -0500

    Add CRUD operations

--- src/main/java/com/cougarneticit/gims/controller/admin/EmployeeSceneController.java
+++ src/main/java/com/cougarneticit/gims/controller/admin/EmployeeSceneController.java
@@ -7,6 +7,7 @@ import com.cougarneticit.gims.model.repos.EmployeeRepo;
 import com.cougarneticit.gims.model.repos.UserRepo;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -24,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.UUID;
 import java.util.regex.Matcher;
@@ -42,9 +44,10 @@ public class EmployeeSceneController extends GIMSController implements Initializ
 
     @FXML private AnchorPane pane;
     @FXML private JFXComboBox<User> userComboBox;
+    @FXML private JFXListView<Employee> employeeListView;
     @FXML private JFXTextField nameTextField, phoneTextField, emailTextField;
-    @FXML private Label nameHelpLabel, phoneHelpLabel, emailHelpLabel;
-    @FXML private JFXButton addEmployeeButton;
+    @FXML private Label nameHelpLabel, phoneHelpLabel, emailHelpLabel, deleteStatusLabel, employeeFormLabel;
+    @FXML private JFXButton addEmployeeButton, editEmployeeButton, deleteEmployeeButton, cancelButton;
 
     public EmployeeSceneController(FxWeaver fxWeaver) {
         super(fxWeaver);
@@ -56,49 +59,19 @@ public class EmployeeSceneController extends GIMSController implements Initializ
         initStage(stage, pane, null, null, null, null, true);
 
         populateComboBox();
-
+        populateEmployeeListView();
 
         addEmployeeButton.setOnAction(e -> {
-            User selectedUser = userComboBox.getSelectionModel().getSelectedItem();
-            String name = nameTextField.getText();
-            String phone = phoneTextField.getText();
-            String email = emailTextField.getText();
-
-            boolean isNameValid = validateName(name);
-            boolean isPhoneValid = validatePhone(phone);
-            boolean isEmailValid = validateEmail(email);
-
-            if (isNameValid && isPhoneValid && isEmailValid) {
-                Employee emp = new Employee(UUID.randomUUID(), selectedUser, name, phone, email);
-                employeeRepo.save(emp);
-
-                nameHelpLabel.setTextFill(Color.web("#5BDDC7"));
-                nameHelpLabel.setText("First Last");
-                nameHelpLabel.setVisible(false);
-                phoneHelpLabel.setTextFill(Color.web("#5BDDC7"));
-                phoneHelpLabel.setText("xxx-xxx-xxxx");
-                phoneHelpLabel.setVisible(false);
-                emailHelpLabel.setTextFill(Color.web("#5BDDC7"));
-                emailHelpLabel.setText("email@domain.com");
-                emailHelpLabel.setVisible(false);
-            }
-            else {
-                if(!isNameValid) {
-                    nameHelpLabel.setTextFill(Color.web("#F73331"));
-                    nameHelpLabel.setText("Invalid format: First Last");
-                    nameHelpLabel.setVisible(true);
-                }
-                else if(!isPhoneValid) {
-                    phoneHelpLabel.setTextFill(Color.web("#F73331"));
-                    phoneHelpLabel.setText("Invalid format: xxx-xxx-xxxx");
-                    phoneHelpLabel.setVisible(true);
-                }
-                else {
-                    emailHelpLabel.setTextFill(Color.web("#F73331"));
-                    emailHelpLabel.setText("Invalid format: email@domain.com");
-                    emailHelpLabel.setVisible(true);
-                }
-            }
+            addEmployee();
+        });
+        cancelButton.setOnAction(e -> {
+            resetEmployeeForm();
+        });
+        editEmployeeButton.setOnAction(e -> {
+            editEmployee();
+        });
+        deleteEmployeeButton.setOnAction(e -> {
+            deleteEmployee();
         });
 
         //Focus listeners for text fields. Show/Hide on focus/un-focus
@@ -107,6 +80,118 @@ public class EmployeeSceneController extends GIMSController implements Initializ
         emailTextField.focusedProperty().addListener((obs, oldVal, newVal) -> emailHelpLabel.setVisible(newVal));
     }
 
+    private void addEmployee() {
+        User selectedUser = userComboBox.getSelectionModel().getSelectedItem();
+        String name = nameTextField.getText();
+        String phone = phoneTextField.getText();
+        String email = emailTextField.getText();
+
+        boolean isNameValid = validateName(name);
+        boolean isPhoneValid = validatePhone(phone);
+        boolean isEmailValid = validateEmail(email);
+
+        if (isNameValid && isPhoneValid && isEmailValid) {
+            Employee emp = new Employee(UUID.randomUUID(), selectedUser, name, phone, email);
+            employeeRepo.save(emp);
+
+            populateEmployeeListView();
+            resetEmployeeForm();
+        }
+        else {
+            if(!isNameValid) {
+                nameHelpLabel.setTextFill(Color.web("#F73331"));
+                nameHelpLabel.setText("Invalid format: First Last");
+                nameHelpLabel.setVisible(true);
+            }
+            else if(!isPhoneValid) {
+                phoneHelpLabel.setTextFill(Color.web("#F73331"));
+                phoneHelpLabel.setText("Invalid format: xxx-xxx-xxxx");
+                phoneHelpLabel.setVisible(true);
+            }
+            else {
+                emailHelpLabel.setTextFill(Color.web("#F73331"));
+                emailHelpLabel.setText("Invalid format: email@domain.com");
+                emailHelpLabel.setVisible(true);
+            }
+        }
+    }
+    private void resetEmployeeForm() {
+        userComboBox.getSelectionModel().clearSelection();
+        userComboBox.setDisable(false);
+
+        employeeFormLabel.setText("Add an Employee");
+        addEmployeeButton.setText("Add Employee");
+
+        nameTextField.clear();
+        phoneTextField.clear();
+        emailTextField.clear();
+
+        nameHelpLabel.setTextFill(Color.web("#5BDDC7"));
+        nameHelpLabel.setText("First Last");
+        nameHelpLabel.setVisible(false);
+        phoneHelpLabel.setTextFill(Color.web("#5BDDC7"));
+        phoneHelpLabel.setText("xxx-xxx-xxxx");
+        phoneHelpLabel.setVisible(false);
+        emailHelpLabel.setTextFill(Color.web("#5BDDC7"));
+        emailHelpLabel.setText("email@domain.com");
+        emailHelpLabel.setVisible(false);
+    }
+    private void editEmployee() {
+        try {
+            addEmployeeButton.setOnAction(e -> {
+                //Submit edits
+                submitEmployeeEdits();
+                //Revert to original event handler
+                addEmployeeButton.setOnAction(ee -> {
+                    addEmployee();
+                });
+            });
+
+            Employee selectedEmployee = employeeListView.getSelectionModel().getSelectedItem();
+
+            //Set combobox and fill text fields
+            for (User user : userComboBox.getItems()) {
+                if (selectedEmployee.getUser_id().equals(user.getUser_id())) {
+                    userComboBox.getSelectionModel().select(user);
+                    userComboBox.setDisable(true);
+                    break;
+                }
+            }
+            nameTextField.setText(selectedEmployee.getName());
+            phoneTextField.setText(selectedEmployee.getPhone());
+            emailTextField.setText(selectedEmployee.getEmail());
+
+            employeeFormLabel.setText("Edit an Employee");
+            addEmployeeButton.setText("Submit Edits");
+        }
+        catch(NullPointerException ex) {
+            //Revert to original event handler
+            addEmployeeButton.setOnAction(e -> {
+                addEmployee();
+            });
+            System.err.println("Nothing selected");
+            //TODO
+        }
+    }
+    private void deleteEmployee() {
+        //TODO add ScheduledExecutorService + Future to schedule status label setVisible(false)
+        //TODO add confirmation popup
+        try {
+            Employee selectedEmployee = employeeListView.getSelectionModel().getSelectedItem();
+            employeeRepo.deleteById(selectedEmployee.getEmployee_id());
+
+            deleteStatusLabel.setTextFill(Color.web("#5BDDC7"));
+            deleteStatusLabel.setText("Deleted!");
+            deleteStatusLabel.setVisible(true);
+
+            populateEmployeeListView();
+        }
+        catch(NullPointerException ex) {
+            deleteStatusLabel.setTextFill(Color.web("#F73331"));
+            deleteStatusLabel.setText("Nothing selected in list");
+            deleteStatusLabel.setVisible(true);
+        }
+    }
     private void populateComboBox() {
         ObservableList<User> userList = FXCollections.observableArrayList();
         if(userRepo.count() != 0) {
@@ -114,10 +199,17 @@ public class EmployeeSceneController extends GIMSController implements Initializ
         }
         userComboBox.getItems().addAll(userList);
     }
+    private void populateEmployeeListView() {
+        ObservableList<Employee> employeeList = FXCollections.observableArrayList();
+
+        if(employeeRepo.count() != 0) {
+            employeeList.addAll(employeeRepo.findAll());
+        }
+        employeeListView.setItems(employeeList.sorted());
+    }
     private boolean validateEmail(String email) {
         EmailValidator emailValidator = EmailValidator.getInstance();
-        boolean isValid = emailValidator.isValid(email);
-        return isValid;
+        return emailValidator.isValid(email);
     }
     private boolean validateName(String name) {
         Pattern pattern = Pattern.compile("^[a-zA-Z\\s]+");
@@ -128,6 +220,17 @@ public class EmployeeSceneController extends GIMSController implements Initializ
         Pattern pattern = Pattern.compile("^(\\d{3}[-]?){2}\\d{4}$");
         Matcher matcher = pattern.matcher(phone);
         return matcher.matches();
+    }
+
+    private void submitEmployeeEdits() {
+        Employee updatedEmployee = new Employee(
+                employeeListView.getSelectionModel().getSelectedItem().getEmployee_id(),
+                employeeListView.getSelectionModel().getSelectedItem().getUser(),
+                nameTextField.getText(), phoneTextField.getText(), emailTextField.getText());
+        employeeRepo.save(updatedEmployee);
+
+        populateEmployeeListView();
+        resetEmployeeForm();
     }
 
     public AnchorPane getScene() { return pane; }
--- src/main/java/com/cougarneticit/gims/model/repos/EmployeeRepo.java
+++ src/main/java/com/cougarneticit/gims/model/repos/EmployeeRepo.java
@@ -3,8 +3,9 @@ package com.cougarneticit.gims.model.repos;
 import com.cougarneticit.gims.model.Employee;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface EmployeeRepo extends CrudRepository<Employee, UUID> {
-
+    List<Employee> findAll();
 }
