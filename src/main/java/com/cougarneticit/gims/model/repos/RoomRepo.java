18000817a5329b4caf3348330fbfdbf75262098a
ddbogdanov <dennis.d.bogdanov@gmail.com>
2021-04-15 04:47:24 -0500

    Add task components to GUI

--- src/main/java/com/cougarneticit/gims/model/repos/RoomRepo.java
+++ src/main/java/com/cougarneticit/gims/model/repos/RoomRepo.java
@@ -4,7 +4,6 @@ import com.cougarneticit.gims.model.Room;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface RoomRepo extends CrudRepository<Room, Character> {
     List<Room> findAll();
