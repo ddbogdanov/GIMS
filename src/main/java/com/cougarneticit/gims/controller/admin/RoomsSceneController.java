acb4f534106eb8b23509696b23c49fa2053a13e6
ddbogdanov <dennis.d.bogdanov@gmail.com>
2021-04-17 01:58:57 -0500

    Add user side error handling

--- src/main/java/com/cougarneticit/gims/controller/admin/RoomsSceneController.java
+++ src/main/java/com/cougarneticit/gims/controller/admin/RoomsSceneController.java
@@ -1,7 +1,9 @@
 package com.cougarneticit.gims.controller.admin;
 
 import com.cougarneticit.gims.controller.common.GIMSController;
+import com.cougarneticit.gims.model.Employee;
 import com.cougarneticit.gims.model.Room;
+import com.cougarneticit.gims.model.common.Priority;
 import com.cougarneticit.gims.model.common.RoomStatus;
 import com.cougarneticit.gims.model.repos.EmployeeRepo;
 import com.cougarneticit.gims.model.repos.RoomRepo;
@@ -15,6 +17,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,11 +41,15 @@ public class RoomsSceneController extends GIMSController implements Initializabl
     EmployeeRepo employeeRepo;
 
     @FXML private AnchorPane pane;
+    @FXML private JFXComboBox<Priority> priorityComboBox;
+    @FXML private JFXComboBox<RoomStatus> roomStatusComboBox;
+    @FXML private JFXComboBox<Room> roomComboBox;
+    @FXML private JFXComboBox<Employee> employeeComboBox;
     @FXML private JFXListView<Room> roomListView;
     @FXML private JFXToggleButton editToggleButton;
     @FXML private JFXTextField roomIdTextField, roomNameTextField, taskNameTextField;
     @FXML private JFXTextArea taskDescriptionTextArea;
-    @FXML private Label roomFormLabel, roomNameLabel, activeTasksLabel, statusLabel, roomIdHelpLabel, roomNameHelpLabel, taskNameHelpLabel, taskDescriptionHelpLabel;
+    @FXML private Label roomFormLabel, roomNameLabel, roomStatusHelpLabel, activeTasksLabel, statusLabel, roomIdHelpLabel, roomNameHelpLabel, taskNameHelpLabel, taskDescriptionHelpLabel;
     @FXML private JFXButton roomFormSubmitButton, roomFormCancelButton, roomFormDeleteButton;
 
     public RoomsSceneController(FxWeaver fxWeaver) {
@@ -57,6 +64,22 @@ public class RoomsSceneController extends GIMSController implements Initializabl
         populateRoomListView();
         roomListView.getSelectionModel().select(0);
         setInfoLabels();
+
+        //Initialize ComboBoxes
+        priorityComboBox.getItems().addAll(Priority.values());
+        roomStatusComboBox.getItems().addAll(RoomStatus.values());
+        roomComboBox.getItems().addAll(roomRepo.findAll());
+        roomComboBox.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(Room room) {
+                return "Suite " + room.getRoomId();
+            }
+            @Override
+            public Room fromString(String s) {
+                return null;
+            }
+        });
+        employeeComboBox.getItems().addAll(employeeRepo.findAll());
 
         editToggleButton.setOnAction(e -> {
             editToggle();
@@ -97,12 +120,10 @@ public class RoomsSceneController extends GIMSController implements Initializabl
             try {
                 roomFormSubmitButton.setOnAction(e -> {
                     submitRoomEdits();
-                    roomFormSubmitButton.setOnAction(ee -> {
-                        submitRoom();
-                    });
                 });
 
                 populateRoomForm();
+                roomIdTextField.setDisable(true);
                 roomFormLabel.setText("Edit a Room");
                 roomFormSubmitButton.setText("Submit");
             }
@@ -110,7 +131,7 @@ public class RoomsSceneController extends GIMSController implements Initializabl
                 roomFormSubmitButton.setOnAction(ee -> {
                     submitRoom();
                 });
-                System.err.println("Nothing selected");
+                System.err.println("Nothing selected - can't edit");
                 //TODO
             }
         }
@@ -119,26 +140,30 @@ public class RoomsSceneController extends GIMSController implements Initializabl
         }
     }
     private void submitRoomEdits() {
-        Room updatedRoom = new Room(
-                roomIdTextField.getText().charAt(0),
-                roomNameTextField.getText(),
-                RoomStatus.VACANT); //TODO: Accept RoomStatus in Room Form
-        roomRepo.save(updatedRoom);
-
-        populateRoomListView();
-        resetRoomForm();
-    }
-    private void submitRoom() {
-        if(!roomRepo.existsById(roomIdTextField.getText().charAt(0))) {
-            Room room = new Room(roomIdTextField.getText().charAt(0), roomNameTextField.getText(), RoomStatus.VACANT);
-            roomRepo.save(room);
+        if(validateRoomForm()) {
+            Room updatedRoom = new Room(
+                    roomIdTextField.getText().charAt(0),
+                    roomNameTextField.getText(),
+                    roomStatusComboBox.getValue());
+            roomRepo.save(updatedRoom);
 
             populateRoomListView();
             resetRoomForm();
         }
-        else {
-            //TODO
-            System.err.println("already exists");
+    }
+    private void submitRoom() {
+        if(validateRoomForm()) {
+            if (!roomRepo.existsById(roomIdTextField.getText().charAt(0))) {
+                Room room = new Room(roomIdTextField.getText().charAt(0), roomNameTextField.getText(), roomStatusComboBox.getValue());
+                roomRepo.save(room);
+
+                populateRoomListView();
+                resetRoomForm();
+            } else {
+                roomIdHelpLabel.setText("An existing room already has this ID");
+                roomIdHelpLabel.setTextFill(Color.web("#F73331"));
+                roomIdHelpLabel.setVisible(true);
+            }
         }
     }
     private void deleteRoom() {
@@ -150,8 +175,7 @@ public class RoomsSceneController extends GIMSController implements Initializabl
             resetRoomForm();
         }
         catch(Exception ex) {
-            //TODO
-            System.err.println("nothing selected in list");
+            System.err.println("Nothing selected in list");
         }
     }
 
@@ -167,10 +191,35 @@ public class RoomsSceneController extends GIMSController implements Initializabl
         Room selectedRoom = roomListView.getSelectionModel().getSelectedItem();
 
         roomIdTextField.setText(String.valueOf(selectedRoom.getRoomId()));
-        roomIdTextField.setDisable(true);
         roomNameTextField.setText(selectedRoom.getRoomName());
+        roomStatusComboBox.getSelectionModel().select(selectedRoom.getStatus());
+    }
+    private boolean validateRoomForm() {
+        if(roomIdTextField.getText().isBlank()) {
+            roomIdHelpLabel.setText("ID cannot be blank");
+            roomIdHelpLabel.setTextFill(Color.web("#F73331"));
+            roomIdHelpLabel.setVisible(true);
+            return false;
+        }
+        else if(roomNameTextField.getText().isBlank()) {
+            roomNameHelpLabel.setText("Name cannot be blank");
+            roomNameHelpLabel.setTextFill(Color.web("#F73331"));
+            roomNameHelpLabel.setVisible(true);
+            return false;
+        }
+        else if(roomStatusComboBox.getSelectionModel().isEmpty()) {
+            roomStatusHelpLabel.setText("Status cannot be blank");
+            roomStatusHelpLabel.setTextFill(Color.web("#F73331"));
+            roomStatusHelpLabel.setVisible(true);
+            return false;
+        }
+        else {
+            return true;
+        }
     }
     private void resetRoomForm() {
+        roomStatusComboBox.getSelectionModel().clearSelection();
+
         editToggleButton.selectedProperty().setValue(false);
         roomFormSubmitButton.setText("Add Room");
 
@@ -180,11 +229,22 @@ public class RoomsSceneController extends GIMSController implements Initializabl
         roomIdTextField.setDisable(false);
         roomNameTextField.clear();
 
+        roomIdHelpLabel.setText("Single Character");
+        roomIdHelpLabel.setTextFill(Color.web("#5BDDC7"));
+        roomIdHelpLabel.setVisible(false);
+        roomNameHelpLabel.setText("120 Character Max");
+        roomNameHelpLabel.setTextFill(Color.web("#5BDDC7"));
+        roomNameHelpLabel.setVisible(false);
+        roomStatusHelpLabel.setText("Room Status");
+        roomStatusHelpLabel.setTextFill(Color.web("#5BDDC7"));
+        roomStatusHelpLabel.setVisible(false);
+
         //Reset event handler
         roomFormSubmitButton.setOnAction(e -> {
             submitRoom();
         });
     }
+
     private void setInfoLabels() {
         Room selectedRoom = roomListView.getSelectionModel().getSelectedItem();
 
